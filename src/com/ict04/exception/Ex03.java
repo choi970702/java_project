ab0edf243d16b7171226b937a376504786762dc6
choi970702 <choi970702@gmail.com>
2021-04-05 17:26:28 +0900

    first

---
+++ src/com/ict04/exception/Ex03.java
@@ -0,0 +1,58 @@
+package com.ict04.exception;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Ex03 
+{
+	public static void main(String[] args) 
+	{
+		// 다중 catch문 : try문의 예외 발생이 여러 가지의 예외가 발생할 경우 사용
+		// 주의사항 : Exception은 사용할땐 반드시 가장 아래쪽 catch문을 사용함
+		// 어떤수를 받아서 나누기를 함
+		
+		Scanner scan = new Scanner(System.in);
+		/*
+		try 
+		{
+			int var = 3;
+			System.out.println("정수를 입력하세요");
+			int su = scan.nextInt();
+			System.out.println("정답 : "+(var/su));
+			
+		} catch (InputMismatchException e) 
+		{
+			System.out.println("정수만 입력 하세요.");
+		} catch(ArithmeticException e)
+		{
+			System.out.println("0 으로는 숫자를 나눌 수 없습니다.");
+		} catch (Exception e) 
+		{
+			System.out.println("제대로 입력 하세요.");
+		}*/
+		
+		try 
+		{
+			int var = 3;
+			System.out.println("정수를 입력하세요");
+			int su = scan.nextInt();
+			System.out.println("정답 : "+(var/su));
+			
+		} catch (Exception e) 
+		{
+			System.out.println("제대로 입력 하세요. " + e);
+		}
+		
+		System.out.println("수고 하셨습니다.");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+}
---
+++ src/com/ict04/exception/Ex05.java
@@ -0,0 +1,49 @@
+package com.ict04.exception;
+
+import java.util.Scanner;
+
+public class Ex05 
+{
+	// throws (예외 양도, 예외 전가)
+	// 예외가 발생하면 예외를 처리하지 않고, 자신을 호출한 곳으로 예외 객체를 전달하는 것
+	// 나중에라도 예외처리(try~catch)를 하는 것이 좋다.
+
+	public static void main(String[] args) 
+	{
+		Ex05 test = new Ex05();
+		Scanner scan = new Scanner(System.in);
+		System.out.println("정수 입력 : ");
+		String msg = scan.next();
+		test.setData(msg);
+		
+
+	}
+	public void setData(String msg)
+	{
+		// msg의 글자의 길이가 한개 이상이면 실행
+		if(msg.length()>=1)
+		{
+			String str = msg.substring(0,1); // 첫 글자만 추출해라
+			prnData(str);
+		}
+		
+		
+	}
+	public void prnData(String str)
+	{
+		int dan = Integer.parseInt(str);
+		System.out.println(dan +" 단");
+		for (int i = 0; i < 10; i++) 
+		{
+			System.out.println(dan +" * "+" = "+(dan*i));
+		}
+		
+		
+	}
+	
+	
+	
+	
+	
+
+}
